d3724d6432e5d35af7ea9df56d24ef87ec91f094
Anita Luo <luo_anita@yahoo.com>
2021-12-23 02:44:11 -0600

    successful initial GET 50 liked songs

--- main.py
+++ main.py
@@ -7,25 +7,17 @@ import os
 from os import path
 import time
 import random
-from similar import similar
-from valence import valence
 
 # authenticate
 authentication=SpotifyOAuth(scope=constants.SPOTIPY_SCOPE, client_id=constants.SPOTIPY_CLIENT_ID, 
     client_secret=constants.SPOTIPY_CLIENT_SECRET, redirect_uri=constants.SPOTIPY_REDIRECT_URI)
 sp = spotipy.Spotify(auth_manager=authentication)
 
-# playlist you want to sort
-pl_id = constants.UNSORTED_PLAYLIST_ID
-offset=0
-response = sp.playlist_items(pl_id, offset=offset, fields='items.track.id,total', additional_types=['track'])
-# track ids array to store all track ids
-trackids=[]
-for items in response['items']:
-    trackids.append(items['track']['id'])
+# get users 'liked songs'
+results = sp.current_user_saved_tracks(limit=50, offset=0)
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
 
-# makes playlist having 2 most similar songs, then each song following similar to previous
-similar(trackids, sp)
-# makes playlist with valence ascending
-valence(trackids, sp)
+
 
